b54e63ce541a5466de5cb1b5f55690e80eb93181
Mohamed Alaa El-deen Kilany <mohamedaed92@gmail.com>
2026-05-07 13:29:18 -0500

    App init

---
+++ src/main/java/com/googleIntegration/gPanel/GPanelApplication.java
@@ -0,0 +1,13 @@
+package com.googleIntegration.gPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googleIntegration/gPanel/GPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googleIntegration.gPanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
